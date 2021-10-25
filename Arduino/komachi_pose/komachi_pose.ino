84352782667009d2519dfc3b491ac0d68a0c16a8
K.Arai <k.arai_payday@outlook.com>
2021-10-25 19:40:40 +0900

    add komachi_pose program

---
+++ Arduino/komachi_pose/komachi_pose.ino
@@ -0,0 +1,22 @@
+#include <M5Atom.h>
+#include <ESP32Servo.h>
+
+
+// servo settings
+Servo servo;
+ESP32PWM pwm
+int servoPin = 21;
+
+// initial position(deg)
+int pos = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
