48545c2024f603cc0c28792d01e704b556f3e33d
K.Arai <k.arai_payday@outlook.com>
2021-10-30 12:05:19 +0900

    :tada:add all features

--- Arduino/komachi_pose/komachi_pose.ino
+++ Arduino/komachi_pose/komachi_pose.ino
@@ -11,8 +11,22 @@ Servo servo;
 ESP32PWM pwm
 int servoPin = 21;
 
+// firebase, wifi settings
+#define FIREBASE_HOST "hogehoge"
+#define FIREBASE_AUTH "hogehogehoge"
+#define WIFI_SSID "hogehogeeee"
+#define WIFI_PASSWORD "hogehogeohogeeee"
+FirebaseData fbd;
+
+
+// servo settings
+Servo servo;
+ESP32PWM pwm;
+int servoPin = 21;
+
 // initial position(deg)
 int pos = 0;
+float pos_fire = 0;
 
 
 void setup() {
@@ -24,13 +38,44 @@ void setup() {
 
   // M5 init
   M5.begin();
+  delay(50);
+
+  // wifi init
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    M5.dis.drawpix(0, 0xff0000);        // RED(not-ready)
+  }
+
+  // firebase init
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
 
   // servo init
   servo.setPeriodHertz(50);
   servo.attach(servoPin, MIN_PLS, MAX_PLS);
+  M5.dis.drawpix(0, 0x0000f0);        // BLUE(ready)
+  servo.write(0);
+  delay(1);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // get pos data
+  Firebase.getFloat(fbd, "/pose/");
+  pos_fire = fbd.to<float>();
+
+  if(pos_fire<=0 && pos_fire>=-45)
+  {
+    // cast
+    pos_fire = pos_fire * (5.0 / 9.0);
+    pos = -(int)pos_fire;
+
+    // move neck
+    servo.write(pos);
+    delay(1);
+  }else{
+    // do nothing
+  }
 
 }
